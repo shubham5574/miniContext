0b7525aa1c07bec0f3af39c088868905d0f2e798
Shubham Kushwaha <shubhamkushwaha38001@gmail.com>
2026-08-09 11:04:54 +0530

    Test GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-
-import './App.css'
-import UsercontextProvider from './Context/UsercontextProvider'
-import Login from './Componente/Login'
-import Profile from './Componente/Profile'
-
 function App() {
-
-
   return (
-   <UsercontextProvider>
-    <h2>context api</h2>
-    <Profile/>
-   <Login/>
-   </UsercontextProvider>
+    <div>
+      <h1>My React App</h1>
+      <p>GitHub Pages is working!</p>
+    </div>
   )
 }
 
