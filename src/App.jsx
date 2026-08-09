ceff33298e2190def75a1b4d8736d10d73b404c5
Shubham Kushwaha <shubhamkushwaha38001@gmail.com>
2026-08-09 12:08:58 +0530

    Update React app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+
+import './App.css'
+import UsercontextProvider from './Context/UsercontextProvider'
+import Login from './Componente/Login'
+import Profile from './Componente/Profile'
+
+
 function App() {
-  return (
-    <div>
-      <h1>My React App</h1>
-      <p>GitHub Pages is working!</p>
-    </div>
-  )
+
+
+
+  return (
+   <UsercontextProvider>
+    <h2>context api</h2>
+    <Profile/>
+   <Login/>
+   </UsercontextProvider>
+  )
 }
 
+
 export default App
+
